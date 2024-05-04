Fix a bug where positions do not increment after the last input token. This is because the input mask used for computing the positions was previously masking away the default padding tokens that follow the input tokens, when only the padding tokens within the input tokens should be masked when used for computing positions.
PiperOrigin-RevId: 630541259
Change-Id: I796ff930973da04bd63863ad64219bab912592b5

gemma/sampler.py
@@ -206,10 +206,15 @@ class Sampler:
         self.vocab.pad_id(),
         dtype=jnp.int32,
     )
+    input_mask = jnp.ones_like(token_buffer, dtype=jnp.bool_)
     for i, (input_ids, num_tokens) in enumerate(
         zip(all_input_ids, num_input_tokens)
     ):
       token_buffer = token_buffer.at[i, :num_tokens].set(input_ids)
+      input_mask = input_mask.at[i, :num_tokens].set(
+          input_ids != self.vocab.pad_id()
+      )
+    positions = transformer_lib.build_positions_from_mask(input_mask)
 
     done = jnp.zeros((bsz,), dtype=jnp.bool_)
 
@@ -220,9 +225,6 @@ class Sampler:
       )
     else:
       logits_buffer = None
-
-    input_mask = token_buffer != self.vocab.pad_id()
-    positions = transformer_lib.build_positions_from_mask(input_mask)
 
     return _SamplingState(
         decoding_step=0,