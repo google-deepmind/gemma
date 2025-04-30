afda242c72164670290d967ef2893b731e8bb339
Etienne Pot <epot@google.com>
2025-04-30 02:40:57 -0700

    Move configs definition to the Gemma models
    PiperOrigin-RevId: 753083923

--- gemma/gm/nn/_gemma.py
+++ gemma/gm/nn/_gemma.py
@@ -16,19 +16,56 @@
 
 from __future__ import annotations
 
-from collections.abc import Callable
-from typing import Any, ClassVar
+import dataclasses
 
+from gemma import modules
 from gemma import transformer
 from gemma.gm.ckpts import _paths
 from gemma.gm.nn import _transformer
+from gemma.multimodal import vision as gemma_vision
+
+_NUM_LAYERS_GEMMA_2B = 18
+_NUM_LAYERS_GEMMA_7B = 28
+_NUM_LAYERS_GEMMA2_2B = 26
+_NUM_LAYERS_GEMMA2_9B = 42
+_NUM_LAYERS_GEMMA2_27B = 46
+_NUM_LAYERS_GEMMA3_1B = 26
+_NUM_LAYERS_GEMMA3_4B = 34
+_NUM_LAYERS_GEMMA3_12B = 48
+_NUM_LAYERS_GEMMA3_27B = 62
+
+
+GEMMA3_ATTENTION_PATTERN = (
+    modules.AttentionType.LOCAL_SLIDING,
+    modules.AttentionType.LOCAL_SLIDING,
+    modules.AttentionType.LOCAL_SLIDING,
+    modules.AttentionType.LOCAL_SLIDING,
+    modules.AttentionType.LOCAL_SLIDING,
+    modules.AttentionType.GLOBAL,
+)
 
 
 class Gemma2_2B(_transformer.Transformer):  # pylint: disable=invalid-name
   """Gemma2 transformer architecture."""
 
-  config: transformer.TransformerConfig = (
-      transformer.TransformerConfig.gemma2_2b()
+  config: transformer.TransformerConfig = transformer.TransformerConfig(
+      num_embed=256128,
+      embed_dim=2304,
+      hidden_dim=9216,
+      num_heads=8,
+      head_dim=256,
+      num_kv_heads=4,
+      final_logit_softcap=30.0,
+      attention_types=(
+          modules.AttentionType.LOCAL_SLIDING,
+          modules.AttentionType.GLOBAL,
+      )
+      * int(_NUM_LAYERS_GEMMA2_2B / 2),
+      use_post_attn_norm=True,
+      use_post_ffw_norm=True,
+      query_pre_attn_norm=transformer.QueryPreAttentionNormalisation.BY_ONE_OVER_SQRT_HEAD_DIM,
+      attn_logits_soft_cap=50.0,
+      sliding_window_size=4096,
   )
 
   INFO = _transformer.ModelInfo(
@@ -40,8 +77,25 @@ class Gemma2_2B(_transformer.Transformer):  # pylint: disable=invalid-name
 class Gemma2_9B(_transformer.Transformer):  # pylint: disable=invalid-name
   """Gemma2 transformer architecture."""
 
-  config: transformer.TransformerConfig = (
-      transformer.TransformerConfig.gemma2_9b()
+  config: transformer.TransformerConfig = transformer.TransformerConfig(
+      num_embed=256128,
+      embed_dim=3584,
+      hidden_dim=14336,
+      num_heads=16,
+      head_dim=256,
+      num_kv_heads=8,
+      final_logit_softcap=30.0,
+      attention_types=(
+          modules.AttentionType.LOCAL_SLIDING,
+          modules.AttentionType.GLOBAL,
+      )
+      * int(_NUM_LAYERS_GEMMA2_9B / 2),
+      use_post_attn_norm=True,
+      use_post_ffw_norm=True,
+      query_pre_attn_norm=transformer.QueryPreAttentionNormalisation.BY_ONE_OVER_SQRT_HEAD_DIM,
+      attn_logits_soft_cap=50.0,
+      sliding_window_size=4096,
+      transpose_gating_einsum=True,
   )
 
   INFO = _transformer.ModelInfo(
@@ -53,8 +107,25 @@ class Gemma2_9B(_transformer.Transformer):  # pylint: disable=invalid-name
 class Gemma2_27B(_transformer.Transformer):  # pylint: disable=invalid-name
   """Gemma2 transformer architecture."""
 
-  config: transformer.TransformerConfig = (
-      transformer.TransformerConfig.gemma2_27b()
+  config: transformer.TransformerConfig = transformer.TransformerConfig(
+      num_embed=256128,
+      embed_dim=4608,
+      hidden_dim=36864,
+      num_heads=32,
+      head_dim=128,
+      num_kv_heads=16,
+      final_logit_softcap=30.0,
+      use_post_attn_norm=True,
+      use_post_ffw_norm=True,
+      attention_types=(
+          modules.AttentionType.LOCAL_SLIDING,
+          modules.AttentionType.GLOBAL,
+      )
+      * int(_NUM_LAYERS_GEMMA2_27B / 2),
+      query_pre_attn_norm=transformer.QueryPreAttentionNormalisation.BY_ONE_OVER_SQRT_EMBED_DIM_DIV_NUM_HEADS,
+      attn_logits_soft_cap=50.0,
+      sliding_window_size=4096,
+      transpose_gating_einsum=True,
   )
 
   INFO = _transformer.ModelInfo(
@@ -66,8 +137,27 @@ class Gemma2_27B(_transformer.Transformer):  # pylint: disable=invalid-name
 class Gemma3_1B(_transformer.Transformer):  # pylint: disable=invalid-name
   """Gemma3 transformer architecture."""
 
-  config: transformer.TransformerConfig = (
-      transformer.TransformerConfig.gemma3_1b()
+  config: transformer.TransformerConfig = transformer.TransformerConfig(
+      final_logit_softcap=None,
+      num_embed=262144,
+      embed_dim=1152,
+      hidden_dim=6 * 1152,
+      num_heads=4,
+      head_dim=256,
+      num_kv_heads=1,
+      use_post_attn_norm=True,
+      use_post_ffw_norm=True,
+      use_qk_norm=True,
+      attention_types=transformer.make_attention_layers_types(
+          GEMMA3_ATTENTION_PATTERN, num_layers=_NUM_LAYERS_GEMMA3_1B
+      ),
+      query_pre_attn_norm=transformer.QueryPreAttentionNormalisation.BY_ONE_OVER_SQRT_HEAD_DIM,
+      attn_logits_soft_cap=None,
+      sliding_window_size=512,
+      transpose_gating_einsum=True,
+      local_base_frequency=10_000,
+      global_base_frequency=1_000_000,
+      vision_encoder=None,
   )
 
   INFO = _transformer.ModelInfo(
@@ -84,23 +174,14 @@ class _Gemma3Base(_transformer.Transformer):
   """
 
   text_only: bool = False  # Whether to skip the vision encoder.
-  config: transformer.TransformerConfig = None  # Auto-set in __post_init__  # pytype: disable=annotation-type-mismatch
-
-  CONFIG_FN: ClassVar[Callable[..., transformer.TransformerConfig]]
-
-  def __init_subclass__(cls, **kwargs: Any):
-    # TODO(epot): Rather than `CONFIG_FN`, instead remove `vision_encoder` from
-    # the config.
-    if 'CONFIG_FN' in cls.__dict__:
-      cls.CONFIG_FN = staticmethod(cls.CONFIG_FN)
-    super().__init_subclass__(**kwargs)
 
   def __post_init__(self):
-    if self.config is None:
+    # Remove the vision encoder if text_only is True.
+    if self.text_only and self.config.vision_encoder is not None:
       object.__setattr__(
           self,
           'config',
-          self.CONFIG_FN(text_only=self.text_only),
+          dataclasses.replace(self.config, vision_encoder=None),
       )
     super().__post_init__()
 
@@ -108,7 +189,30 @@ class _Gemma3Base(_transformer.Transformer):
 class Gemma3_4B(_Gemma3Base):  # pylint: disable=invalid-name
   """Gemma3 transformer architecture."""
 
-  CONFIG_FN = transformer.TransformerConfig.gemma3_4b
+  config: transformer.TransformerConfig = transformer.TransformerConfig(
+      final_logit_softcap=None,
+      num_embed=262_144,
+      embed_dim=2560,
+      hidden_dim=2560 * 8 // 2,
+      num_heads=8,
+      head_dim=256,
+      num_kv_heads=4,
+      use_post_attn_norm=True,
+      use_post_ffw_norm=True,
+      use_qk_norm=True,
+      attention_types=transformer.make_attention_layers_types(
+          GEMMA3_ATTENTION_PATTERN, num_layers=_NUM_LAYERS_GEMMA3_4B
+      ),
+      query_pre_attn_norm=transformer.QueryPreAttentionNormalisation.BY_ONE_OVER_SQRT_HEAD_DIM,
+      attn_logits_soft_cap=None,
+      sliding_window_size=1024,
+      transpose_gating_einsum=True,
+      local_base_frequency=10_000,
+      global_base_frequency=1_000_000,
+      global_scale_factor=8.0,
+      vision_encoder=gemma_vision.SigLiPFromPatches(),
+  )
+
   INFO = _transformer.ModelInfo(
       tokenizer_version=3,
       # default_ckpt=_paths.CheckpointPath.GEMMA3_4B_IT,
@@ -117,8 +221,29 @@ class Gemma3_4B(_Gemma3Base):  # pylint: disable=invalid-name
 
 class Gemma3_12B(_Gemma3Base):  # pylint: disable=invalid-name
   """Gemma3 transformer architecture."""
-
-  CONFIG_FN = transformer.TransformerConfig.gemma3_12b
+  config: transformer.TransformerConfig = transformer.TransformerConfig(
+      final_logit_softcap=None,
+      num_embed=262144,
+      embed_dim=30 * 128,
+      hidden_dim=8 * 30 * 128 // 2,
+      num_heads=16,
+      head_dim=256,
+      num_kv_heads=8,
+      use_post_attn_norm=True,
+      use_post_ffw_norm=True,
+      use_qk_norm=True,
+      attention_types=transformer.make_attention_layers_types(
+          GEMMA3_ATTENTION_PATTERN, num_layers=_NUM_LAYERS_GEMMA3_12B
+      ),
+      query_pre_attn_norm=transformer.QueryPreAttentionNormalisation.BY_ONE_OVER_SQRT_HEAD_DIM,
+      attn_logits_soft_cap=None,
+      sliding_window_size=1024,
+      transpose_gating_einsum=True,
+      local_base_frequency=10_000,
+      global_base_frequency=1_000_000,
+      global_scale_factor=8.0,
+      vision_encoder=gemma_vision.SigLiPFromPatches(),
+  )
   INFO = _transformer.ModelInfo(
       tokenizer_version=3,
       # default_ckpt=_paths.CheckpointPath.GEMMA3_12B_IT,
@@ -127,8 +252,29 @@ class Gemma3_12B(_Gemma3Base):  # pylint: disable=invalid-name
 
 class Gemma3_27B(_Gemma3Base):  # pylint: disable=invalid-name
   """Gemma3 transformer architecture."""
-
-  CONFIG_FN = transformer.TransformerConfig.gemma3_27b
+  config: transformer.TransformerConfig = transformer.TransformerConfig(
+      final_logit_softcap=None,
+      num_embed=262144,
+      embed_dim=5376,
+      hidden_dim=5376 * 8 // 2,
+      num_heads=32,
+      head_dim=128,
+      num_kv_heads=16,
+      use_post_attn_norm=True,
+      use_post_ffw_norm=True,
+      use_qk_norm=True,
+      attention_types=transformer.make_attention_layers_types(
+          GEMMA3_ATTENTION_PATTERN, num_layers=_NUM_LAYERS_GEMMA3_27B
+      ),
+      query_pre_attn_norm=transformer.QueryPreAttentionNormalisation.BY_ONE_OVER_SQRT_EMBED_DIM_DIV_NUM_HEADS,
+      attn_logits_soft_cap=None,
+      sliding_window_size=1024,
+      transpose_gating_einsum=True,
+      local_base_frequency=10_000,
+      global_base_frequency=1_000_000,
+      global_scale_factor=8.0,
+      vision_encoder=gemma_vision.SigLiPFromPatches(),
+  )
   INFO = _transformer.ModelInfo(
       tokenizer_version=3,
       # default_ckpt=_paths.CheckpointPath.GEMMA3_27B_IT,
