45d1d91d6459fbcf1d6f2e6ca73feffa38cfea1b
Etienne Pot <epot@google.com>
2025-04-30 07:00:55 -0700

    Add tests for the examples
    PiperOrigin-RevId: 753151702

--- gemma/gm/tests/examples_test.py
+++ gemma/gm/tests/examples_test.py
@@ -12,16 +12,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pickle
-
 from gemma import gm
+from gemma.examples import seq2seq
+from kauldron import konfig
+import tensorflow_datasets as tfds
 
 # Activate the fixture
 use_hermetic_tokenizer = gm.testing.use_hermetic_tokenizer
 
 
-def test_pickle():
-  tokenizer = gm.text.Gemma3Tokenizer()
-  tokenizer.encode('Hello world!')  # Trigger the lazy-loading of the tokenizer.
+def test_examples():
+  cfg = seq2seq.get_config()
+  with konfig.mock_modules():
+    cfg.model = gm.testing.DummyGemma(
+        tokens='batch.input',
+    )
+  cfg.train_ds.num_workers = 0  # Disable multi-processing in tests.
+  cfg.workdir = '/tmp/gemma_test'
 
-  pickle.dumps(tokenizer)
+  trainer = konfig.resolve(cfg)
+
+  # Resolve the training step, including the metrics, losses,...
+  with tfds.testing.mock_data(num_examples=10):
+    _ = trainer.context_specs
